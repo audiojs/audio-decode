ab8ab2d8ee87792da8a23b6779c8a3171f1f2ddc
Dmitry <df.creative@gmail.com>
2017-04-30 01:28:32 -0400

    Update audio-context

--- browser.js
+++ browser.js
@@ -17,7 +17,7 @@ function decode (buffer, opts, cb) {
 
 	if (!opts) opts = {};
 
-	let ctx = opts.context || context;
+	let ctx = opts.context || context();
 
 	return ctx.decodeAudioData(buffer, (buf) => {
 		cb && cb(null, buf);
